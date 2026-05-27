411e9148a0ba2a885a5afde88489c111c555940a
SergioK <sergio-go@mail.ru>
2026-05-27 15:30:02 +0300

    feat: add app and application

---
+++ src/main/java/ru/kurbanov/presentation/exceptionhandlers/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package ru.kurbanov.presentation.exceptionhandlers;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.kurbanov.domain.exceptions.DomainValidationException;
+import ru.kurbanov.domain.exceptions.IncompatibleComponentException;
+import ru.kurbanov.presentation.dto.responses.ErrorResponseDto;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGenericException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("Internal server error", e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("Not found", e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(DomainValidationException.class)
+    public ResponseEntity<ErrorResponseDto> handleDomainValidationException(DomainValidationException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("Bad request", e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(IncompatibleComponentException.class)
+    public ResponseEntity<ErrorResponseDto> handleIncompatibleComponentException(IncompatibleComponentException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto("Bad request", e.getMessage(), LocalDateTime.now()));
+    }
+}
