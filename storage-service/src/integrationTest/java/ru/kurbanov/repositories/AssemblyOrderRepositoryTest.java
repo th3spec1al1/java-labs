ae833facf992459a51cc47e0a5dedc5ab3b61f8a
th3spec1al1 <th3spec1al1.official@gmail.com>
2026-09-02 14:44:53 +0300

    test: change import in AssemblyOrderRepositoryTest

--- storage-service/src/integrationTest/java/ru/kurbanov/repositories/AssemblyOrderRepositoryTest.java
+++ storage-service/src/integrationTest/java/ru/kurbanov/repositories/AssemblyOrderRepositoryTest.java
@@ -1,6 +1,6 @@
 package ru.kurbanov.repositories;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
