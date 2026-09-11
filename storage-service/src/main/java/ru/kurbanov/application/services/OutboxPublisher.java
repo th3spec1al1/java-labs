53bd410229b47647808898b63b4c74c4cb9543ad
th3spec1al1 <th3spec1al1.official@gmail.com>
2026-09-11 04:56:28 +0300

    feat(storage-service): add traceId propagation via mdc, outbox and grpc

--- storage-service/src/main/java/ru/kurbanov/application/services/OutboxPublisher.java
+++ storage-service/src/main/java/ru/kurbanov/application/services/OutboxPublisher.java
@@ -7,7 +7,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.kurbanov.application.abstractions.repositories.jpa.JpaOutboxEventRepository;
+import ru.kurbanov.infrastructure.persistence.jpa.JpaOutboxEventRepository;
 import ru.kurbanov.infrastructure.persistence.jpa.model.OutboxEventEntity;
 
 import java.time.Instant;
