667dccc21765038be9e8b6e690759deb85fd5eb8
th3spec1al1 <th3spec1al1.official@gmail.com>
2026-09-12 00:37:46 +0300

    feat: add /api/v1/health endpoint without authentification

--- order-service/src/main/java/ru/kurbanov/config/SecurityConfig.java
+++ order-service/src/main/java/ru/kurbanov/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/actuator/health")
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/actuator/health", "/api/v1/health")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
